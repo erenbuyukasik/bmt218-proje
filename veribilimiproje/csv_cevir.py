87c0925e0d57c6e105a80fecd19b6409648d9b17
Eren Büyükaşık <148475264+erenbuyukasik@users.noreply.github.com>
2025-05-12 11:06:10 +0300

    Delete veribilimiproje directory

--- veribilimiproje/csv_cevir.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Excel dosyasını oku
-df = pd.read_excel('Dry_Bean_Dataset.xlsx')
-
-# CSV formatında kaydet
-df.to_csv('Dry_Bean_Dataset.csv', index=False)
-
-print("Excel dosyası başarıyla CSV formatına çevrildi!")
